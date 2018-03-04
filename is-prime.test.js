d6b0d4465940235ca3d658d53941e77e67825c67
Philip Boardman <dustykeyboard@brd.mn>
2018-03-05 08:21:35 +1100

    Apply test arguements to Test fn and reduce success

--- is-prime.test.js
+++ is-prime.test.js
@@ -2,10 +2,14 @@ const Test = require('./test')
 const isPrime = require('./is-prime')
 
 const tests = [
-  Test('0 is Prime', isPrime(0), false),
-  Test('1 is Prime', isPrime(1), false),
-  Test('1 is Prime', isPrime(2), true),
-  Test('16 is Prime', isPrime(16), false),
-  Test('17 is Prime', isPrime(17), true),
-  Test('7919 is Prime', isPrime(7919), true),
+  ['0 is Prime', isPrime(0), false],
+  ['1 is Prime', isPrime(1), false],
+  ['1 is Prime', isPrime(2), true],
+  ['16 is Prime', isPrime(16), false],
+  ['17 is Prime', isPrime(17), true],
+  ['7919 is Prime', isPrime(7919), true],
 ]
+const success = tests.reduce(
+  (pass, test) => pass && Test.apply(this, test),
+  true,
+)
