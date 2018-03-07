c4b21252c26136f30a3181f347b8d34b6c88ae25
Philip Boardman <dustykeyboard@brd.mn>
2018-03-07 22:25:16 +1100

    Add red & green text colour to Test Pass/Fail messages

--- test.js
+++ test.js
@@ -2,13 +2,19 @@ const equal = function defaultComparison(a, b) {
   return a.toString() === b.toString()
 }
 
+const colors = {
+  Reset: '\x1b[0m',
+  Red: '\x1b[31m',
+  Green: '\x1b[32m',
+}
+
 const Test = function Test(name, result, expected) {
   const pass = equal('a', 'b')
   if (equal(expected, result)) {
-    console.log('Pass -', name)
+    console.log(`${colors.Green}Pass${colors.Reset} -`, name)
     return true
   } else {
-    console.log('Fail -', name)
+    console.log(`${colors.Red}Fail${colors.Reset} -`, name)
     console.log('       Expected', expected)
     console.log('       Received', result)
     return false
